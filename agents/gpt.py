e0d7e3ad45b05f4a4ee319874c769d7887a92627
Anthony Costarelli <acostarelli@olin.edu>
2023-12-29 11:51:04 -0500

    added simple babble and prune

--- agents/gpt.py
+++ agents/gpt.py
@@ -101,6 +101,9 @@ class GPT4Text(OpenAITextAgent):
     openai_model : str = "gpt-4-1106-preview"
     agent_type_id : str = "gpt-4"
 
+# could be useful to create a bit of a wrapper around the openai api
+# or at least a more general openaitextagent model
+
 @dataclass
 class ChainOfThought(Agent):
     """Simple CoT agent"""
@@ -147,7 +150,6 @@ class ChainOfThought(Agent):
             model=self.openai_model,
             messages=messages
         ).choices[0].message.content
-        #print("cot", response)
         messages.append({"role": "assistant", "content": response})
 
         prompt = "Now, choose your action. To summarize, you must return json with an 'action' key which contains one of the following valid actions:\n"
@@ -176,6 +178,87 @@ class ChainOfThought(Agent):
             print(f"WARNING: GPT returned an a random action after {self.max_retries} tries")
             return Action(action_id=None)
 
-        print("\n".join([m["content"] for m in messages]))
-        print("################")
+        return Action(action_id=result["action"], openended_response=result.get("openended_response"))
+
+# todo: only allow # of retries
+# todo: maybe CoT critique
+
+@dataclass
+class BabbleAndPrune(Agent):
+    """Agent critiques own action."""
+    openai_model : str = "gpt-3.5-turbo-1106"
+    agent_type_id : str = "bap"
+    system_message : str = "You are an agent playing a game. Select the action that maximizes your probability of winning."
+    max_retries : int = 3
+
+    def take_action(self, rules: Rules, observation: Observation, available_actions: AvailableActions, show_state : bool):
+        valid_actions = []
+        prompt = f"You are playing a game called {rules.title}. The rules are as follows:\n{rules.summary}\n"
+        if rules.additional_details != None:
+            prompt += "The following are headings with additional information about the rules that you can expand by taking the action Explain(<heading key>).\n"
+            details_dict = {f"H{i+1}": topic for i, topic in rules.additional_details}
+            prompt += json.dumps(details_dict, indent=4)
+            valid_actions.extend(f"Explain({h})" for h in list(details_dict.keys()))
+
+        prompt += f"\n# Observation\nThe following describes the current state of the game:\n{observation.text}\n"
+        assert available_actions.predefined != {} or available_actions.openended != {}
+        prompt += f"\n# Actions\n"
+        prompt += f"{available_actions.instructions}\n"
+
+        if len(list(available_actions.openended.keys())) > 0:
+            prompt += action_format_instructions_with_openended
+            prompt += "The following are openended actions you can take\n"
+            prompt += str(list(available_actions.openended.keys())) + "\n"
+            valid_actions += list(available_actions.openended)
+        else:
+            prompt += action_format_instructions_no_openended
+
+        if len(list(available_actions.predefined.keys())) > 0:
+            prompt += "The following are predefined actions you can take:\n"
+            prompt += str(list(available_actions.predefined)) + "\n"
+            valid_actions += list(available_actions.predefined)
+        if any([available_actions.predefined[action] != None for action in list(available_actions.predefined.keys()) + list(available_actions.openended.keys())]):
+            prompt += "Return the action Explain(<action>) to receive additional info about what any of the above actions do.\n"
+
+        prompt += "\nTo summarize, you must return json with an 'action' key which contains one of the following valid actions:\n"
+        prompt += str(list(valid_actions))
+
+        messages = [
+            {"role": "system", "content": self.system_message},
+            {"role": "user", "content": prompt},
+        ]
+
+        result = None
+        for _ in range(self.max_retries):
+            while True:
+                response = openai_client.chat.completions.create(
+                    model=self.openai_model,
+                    response_format={ "type": "json_object" },
+                    messages=messages
+                ).choices[0].message.content
+                messages.append({"role": "assistant", "content": response})
+                messages.append({"role": "user", "content": "Evaluate this action. If you agree it is a good action, respond with exactly 'yes'. If you don't agree, respond with exactly 'no'"})
+                critique = openai_client.chat.completions.create(
+                    model=self.openai_model,
+                    messages=messages
+                ).choices[0].message.content
+                messages.append({"role": "assistant", "content": critique})
+
+                if critique == "yes":
+                    break
+
+            action = ast.literal_eval(response)
+            if action["action"] in valid_actions:
+                result = action
+                break
+            else:
+                error_message = f"{action['action']} is not one of the valid actions. "
+                error_message += "As a reminder, the valid actions are as follows:\n"
+                error_message += f"{str(list(valid_actions))}\n"
+                error_message += "Please return a json with the key 'action' with the action you choose and (optionally) the key 'openended_response' if you select openended response action."
+                messages.append({"role": "user", "content": error_message})
+        if result == None:
+            print(f"WARNING: GPT returned an a random action after {self.max_retries} tries")
+            return Action(action_id=None)
+
         return Action(action_id=result["action"], openended_response=result.get("openended_response"))
