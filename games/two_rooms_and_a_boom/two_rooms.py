988d649c04175e87c06fbd0fc317a6fc044044f7
mat <matdallen@gmail.com>
2024-01-05 22:33:48 -0800

    added cards and rooms

--- games/two_rooms_and_a_boom/two_rooms.py
+++ games/two_rooms_and_a_boom/two_rooms.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from api.classes import Observation, Action, Agent, AvailableActions, Game, Rules
 import random
 
+
 @dataclass
 class TwoRoomsAndaBoom(Game):
     rules : Rules = Rules(
@@ -9,7 +10,35 @@ class TwoRoomsAndaBoom(Game):
         summary="Blue team wants to keep the president safe, red team wants to blow up the president at the end of the third turn.",
         additional_details = None
     )
+
+   # Game "environment" definitions 
+    class Card:
+        def __init__(self, identifier, team, special_character):
+            self.identifier = identifier #for ease of trading back and forth
+            self.team = team # red / blue
+            self.special_character = special_character # Pres / Bomber
+            
+        def __repr__(self):
+            return f"Card({self.team}, {self.special_character})"
+
+    class Room:
+        def __init__(self, identifier):
+            self.cards = []
+            self.identifier = identifier
+
+        def add_card(self, card):
+            self.cards.append(card)
+
+        def show_cards(self):
+            return self.cards
+
+        # TODO: It'd be nice to have a one for one trade where you pass in the card that belongs to a specific room and swap that with the card from the other room.
+        def trade_card(room_a_receive_card, room_b_receive_card):
+            return
+
+    # attribute definitions  
     id : str = "two_rooms_and_a_boom"
+    rooms: list[Room] = field(default_factory=lambda: [TwoRoomsAndaBoom.Room("0"), TwoRoomsAndaBoom.Room("1")])
 
     def init_game(self, agent1 : Agent, agent2 : Agent):        
         self.states = [{
@@ -18,11 +47,18 @@ class TwoRoomsAndaBoom(Game):
         
         # Uncertain if "teams" is the correct way to assign teams (2024.01.02)
         self.agent_data = {
-            0: {"team": "red"},
-            1: {"team": "blue"}
+            0: {"team": "blue"},
+            1: {"team": "red"}
         }
         
         self.agents = [agent1(team_id = 0, agent_id = 0), agent2(team_id = 1, agent_id = 1)]
+        
+        # init cards
+        cards_per_room = 6
+        for i in range(1, cards_per_room+1):
+            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"Card{i}", "Blue", ""))
+            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"Card{ i + cards_per_room }", "Red", ""))
+
 
         self.winning_team = None
         if self.show_state:
@@ -32,28 +68,18 @@ class TwoRoomsAndaBoom(Game):
     
     def get_observation(self, agent : Agent): 
         return
-    
+
+    def display_rooms(self):
+        for index,room in enumerate(self.rooms):
+            print(f"Room {index}: {room.show_cards()}")
 
     def play(self):
-        player_1 = self.agents[0]
-        player_2 = self.agents[1]
+#        player_1 = self.agents[0]
+#        player_2 = self.agents[1]
         
-        
-        # while True:
-        #     # Player 1 moves
-        #     observation, available_actions = self.get_observation(player_1)
-        #     action = player_1.take_action(self.rules, observation, available_actions, show_state=self.show_state)
-        #     self.update(action, available_actions, player_1)
-        #     if self.game_is_over:
-        #         break
-
-        #     # Player 2 moves
-        #     observation, available_actions = self.get_observation(player_2)
-        #     action = player_2.take_action(self.rules, observation, available_actions, show_state=self.show_state)
-        #     self.update(action, available_actions, player_2)
-        #     if self.game_is_over:
-        #         break
-
+        TwoRoomsAndaBoom.display_rooms(self)
         self.winning_team = None
+
+        
 
         return (0.5, 0.5) if self.winning_team == None else (float(self.winning_team == 0), float(self.winning_team == 1))
