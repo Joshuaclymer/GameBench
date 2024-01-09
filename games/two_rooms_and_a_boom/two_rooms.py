f9cba3b2865b00328c4478c8665eddf4cfa8e0b7
mat <matdallen@gmail.com>
2024-01-08 16:49:46 -0800

    added 'determine_winner' function

--- games/two_rooms_and_a_boom/two_rooms.py
+++ games/two_rooms_and_a_boom/two_rooms.py
@@ -5,13 +5,7 @@ import random
 
 @dataclass
 class TwoRoomsAndaBoom(Game):
-    rules : Rules = Rules(
-        title="Two Rooms and a Boom",
-        summary="Blue team wants to keep the president safe, red team wants to blow up the president at the end of the third turn.",
-        additional_details = None
-    )
-
-   # Game "environment" definitions 
+   ## Game "environment" definitions 
     class Card:
         def __init__(self, identifier, team, special_character):
             self.identifier = identifier #for ease of trading back and forth
@@ -36,10 +30,16 @@ class TwoRoomsAndaBoom(Game):
         def trade_card(room_a_receive_card, room_b_receive_card):
             return
 
-    # attribute definitions  
+    ## attribute definitions  
+    rules : Rules = Rules(
+        title="Two Rooms and a Boom",
+        summary="Blue team wants to keep the president safe, red team wants to blow up the president at the end of the third turn.",
+        additional_details = None
+    )
     id : str = "two_rooms_and_a_boom"
     rooms: list[Room] = field(default_factory=lambda: [TwoRoomsAndaBoom.Room("0"), TwoRoomsAndaBoom.Room("1")])
 
+    ## Function definitions
     def init_game(self, agent1 : Agent, agent2 : Agent):        
         self.states = [{
             "turn" : 0
@@ -47,21 +47,23 @@ class TwoRoomsAndaBoom(Game):
         
         # Uncertain if "teams" is the correct way to assign teams (2024.01.02)
         self.agent_data = {
-            0: {"team": "blue"},
-            1: {"team": "red"}
+            0: {"team": "Blue"},
+            1: {"team": "Red"}
         }
         
         self.agents = [agent1(team_id = 0, agent_id = 0), agent2(team_id = 1, agent_id = 1)]
+        print(self.agents)
+        print(self.agent_data)
         
         # init cards
         cards_per_room = 6
         for i in range(1, cards_per_room+1):
-            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"Card{i}", "Blue", ""))
-            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"Card{ i + cards_per_room }", "Red", ""))
+            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"Card{i}", "Blue", "regular"))
+            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"Card{ i + cards_per_room }", "Red", "regular"))
 
         # assign special characters
-        self.rooms[0].cards[ random.randrange(0, cards_per_room + 1) ].special_character = "President"
-        self.rooms[1].cards[ random.randrange(0, cards_per_room + 1) ].special_character = "Bomber"
+        self.rooms[0].cards[ random.randrange(0, cards_per_room ) ].special_character = "President"
+        self.rooms[1].cards[ random.randrange(0, cards_per_room ) ].special_character = "Bomber"
 
         self.winning_team = None
         if self.show_state:
@@ -83,6 +85,18 @@ class TwoRoomsAndaBoom(Game):
         TwoRoomsAndaBoom.display_rooms(self)
         self.winning_team = None
 
-        
 
-        return (0.5, 0.5) if self.winning_team == None else (float(self.winning_team == 0), float(self.winning_team == 1))
+        def determine_winner():
+            pres_location = ""
+            bomber_location = ""
+            for room_index in range(2):
+                for card in self.rooms[room_index].cards:
+                    if card.special_character == "President":
+                        pres_location = self.rooms[room_index]
+                    elif card.special_character == "Bomber":
+                        bomber_location = self.rooms[room_index]
+
+            winning_score = (0,1) if bomber_location == pres_location else (1,0) 
+            return winning_score
+
+        return determine_winner()
