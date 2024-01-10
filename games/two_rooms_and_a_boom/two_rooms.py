fa2c9f998c12e67b6c250a975a86144b9a4e8fb1
mat <matdallen@gmail.com>
2024-01-10 13:59:00 -0800

    added random assigning of rooms

--- games/two_rooms_and_a_boom/two_rooms.py
+++ games/two_rooms_and_a_boom/two_rooms.py
@@ -6,11 +6,11 @@ import pdb
 
 @dataclass
 class TwoRoomsAndaBoom(Game):
-   ## Game "environment" definitions 
+   ## Game Classes ##
     class Card:
         def __init__(self, identifier, team, special_character):
             self.identifier = identifier #for ease of trading back and forth
-            self.team = team # blue / red
+            self.team = team # blue / red. Also keeping convention of "Blue / Red" team as ordering
             self.special_character = special_character # Pres / Bomber
             
         def __repr__(self):
@@ -30,9 +30,7 @@ class TwoRoomsAndaBoom(Game):
         def remove_card(self, card):
             self.cards.remove(card)
 
-
-
-    ## attribute definitions  
+    ## Attribute definitions ##
     rules : Rules = Rules(
         title="Two Rooms and a Boom",
         summary="Blue team wants to keep the president safe, red team wants to blow up the president at the end of the third turn.",
@@ -41,7 +39,7 @@ class TwoRoomsAndaBoom(Game):
     id : str = "two_rooms_and_a_boom"
     rooms: list[Room] = field(default_factory=lambda: [TwoRoomsAndaBoom.Room("0"), TwoRoomsAndaBoom.Room("1")])
 
-    # TODO: It'd be nice to have a one for one trade where you pass in the card that belongs to a specific room and swap that with the card from the other room.
+    ## Function definitions ## 
     def trade_card(self, room_0_give_card, room_1_give_card):
         # add to deck
         self.rooms[0].add_card(room_1_give_card)
@@ -52,7 +50,6 @@ class TwoRoomsAndaBoom(Game):
         self.rooms[1].remove_card(room_1_give_card)
         return
 
-    ## Function definitions
     def init_game(self, agent1 : Agent, agent2 : Agent):        
         self.states = [{
             "turn" : 0
@@ -66,37 +63,52 @@ class TwoRoomsAndaBoom(Game):
         
         self.agents = [agent1(team_id = 0, agent_id = 0), agent2(team_id = 1, agent_id = 1)]
         
-        # init cards
+        # assign cards to rooms, special chars, and shuffle rooms
         cards_per_room = 6
+        
         for i in range(1, cards_per_room+1):
             self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"Card{i}", "Blue", "regular"))
             self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"Card{ i + cards_per_room }", "Red", "regular"))
 
         # assign special characters
-        self.rooms[0].cards[ random.randrange(0, cards_per_room ) ].special_character = "President"
-        self.rooms[1].cards[ random.randrange(0, cards_per_room ) ].special_character = "Bomber"
+        self.rooms[0].cards[random.randrange(0, cards_per_room)].special_character = "President"
+        self.rooms[1].cards[random.randrange(0, cards_per_room)].special_character = "Bomber"
 
-        self.winning_team = None
+        # shuffle
+        for i in range(20):
+            room_0_card = self.rooms[0].cards[random.randrange(0, cards_per_room)]
+            room_1_card = self.rooms[1].cards[random.randrange(0, cards_per_room)]
+            self.trade_card(room_0_card, room_1_card) 
         return
     
-    def get_observation(self, agent : Agent): 
-        return
+    def get_observation(self, agent : Agent): # do I need the -> Tuple[Observation, AvailableActions]: ???
+        room_data_0 = self.rooms[0].show_cards() # not perfect, still needs to hide other team info
+        observation_0 = Observation(text=room_data_0) # going with perform the observation for both teams at same time
 
-    def display_rooms(self):
+        room_data_1 = self.rooms[1].show_cards()
+        observation_1 = Observation(text=room_data_1)       
+
+        available_actions = AvailableActions(
+             instructions = f"Pick a random index from the following list to trade.", # I think it's here, but Leader Instructions would be "return your answer as the index of the item in your room you want to send to the other room"
+             predefined = {
+                ""  
+                 },
+             openended = {}
+        )
+        return observation, available_actions
+
+    def display_rooms(self): 
         for index,room in enumerate(self.rooms):
             print(f"Room {index}: {room.show_cards()}")
 
     def play(self):
-#        player_1 = self.agents[0]
-#        player_2 = self.agents[1]
-        
-        TwoRoomsAndaBoom.display_rooms(self)
         self.winning_team = None
 
 
         def determine_winner():
             pres_location = ""
             bomber_location = ""
+
             for room_index in range(2):
                 for card in self.rooms[room_index].cards:
                     if card.special_character == "President":
@@ -111,12 +123,16 @@ class TwoRoomsAndaBoom(Game):
                 print("Blue won!")
             return winning_score
 
-        determine_winner()
-        self.trade_card(self.rooms[0].cards[2], self.rooms[1].cards[2])
-        determine_winner()
         #pdb.set_trace()
 
-
-
+        ## Begin playing of rounds ## 
+        print("Begin game")
+        self.display_rooms()
+        for i in range(3): # always only three rounds, right?
+            print(f"Round {i+1}")
+            blue_room_trade = self.rooms[0].cards[2] # this card number will be replaced with the Leader Agent's decision
+            red_room_trade  = self.rooms[1].cards[2]
+            self.trade_card(blue_room_trade, red_room_trade)
+            self.display_rooms()
 
         return determine_winner()
