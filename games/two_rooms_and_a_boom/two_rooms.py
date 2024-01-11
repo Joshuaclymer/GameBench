868d5a3baeeda63c0d55d737d3044219c9ec1b2b
mat <matdallen@gmail.com>
2024-01-10 17:57:14 -0800

    agents are trading cards

--- games/two_rooms_and_a_boom/two_rooms.py
+++ games/two_rooms_and_a_boom/two_rooms.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from api.classes import Observation, Action, Agent, AvailableActions, Game, Rules
+from typing import List, Dict, Optional, Tuple
 import random
 import pdb
 
@@ -33,11 +34,16 @@ class TwoRoomsAndaBoom(Game):
     ## Attribute definitions ##
     rules : Rules = Rules(
         title="Two Rooms and a Boom",
-        summary="Blue team wants to keep the president safe, red team wants to blow up the president at the end of the third turn.",
+        summary="""
+        Two teams, Blue and Red, have opposing goals. At the end of three rounds the Red team wants to have both the President and the Bomber in the same room, while Blue team wants them to be in opposite rooms.
+        Each round will allow the Leader of each room to trade 'hostages' in order to find out who the President and Bomber are and use that info to acheive their team's mission.
+        Find out information by talking to other hostages in the room.
+        """,
         additional_details = None
     )
     id : str = "two_rooms_and_a_boom"
     rooms: list[Room] = field(default_factory=lambda: [TwoRoomsAndaBoom.Room("0"), TwoRoomsAndaBoom.Room("1")])
+    cards_per_room: int = 6
 
     ## Function definitions ## 
     def trade_card(self, room_0_give_card, room_1_give_card):
@@ -64,38 +70,35 @@ class TwoRoomsAndaBoom(Game):
         self.agents = [agent1(team_id = 0, agent_id = 0), agent2(team_id = 1, agent_id = 1)]
         
         # assign cards to rooms, special chars, and shuffle rooms
-        cards_per_room = 6
         
-        for i in range(1, cards_per_room+1):
-            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"Card{i}", "Blue", "regular"))
-            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"Card{ i + cards_per_room }", "Red", "regular"))
+        for i in range(1, self.cards_per_room+1):
+            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"{i}", "Blue", "regular"))
+            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"{ i + self.cards_per_room }", "Red", "regular"))
 
         # assign special characters
-        self.rooms[0].cards[random.randrange(0, cards_per_room)].special_character = "President"
-        self.rooms[1].cards[random.randrange(0, cards_per_room)].special_character = "Bomber"
+        self.rooms[0].cards[random.randrange(0, self.cards_per_room)].special_character = "President"
+        self.rooms[1].cards[random.randrange(0, self.cards_per_room)].special_character = "Bomber"
 
         # shuffle
         for i in range(20):
-            room_0_card = self.rooms[0].cards[random.randrange(0, cards_per_room)]
-            room_1_card = self.rooms[1].cards[random.randrange(0, cards_per_room)]
+            room_0_card = self.rooms[0].cards[random.randrange(0, self.cards_per_room)]
+            room_1_card = self.rooms[1].cards[random.randrange(0, self.cards_per_room)]
             self.trade_card(room_0_card, room_1_card) 
         return
     
-    def get_observation(self, agent : Agent): # do I need the -> Tuple[Observation, AvailableActions]: ???
-        room_data_0 = self.rooms[0].show_cards() # not perfect, still needs to hide other team info
-        observation_0 = Observation(text=room_data_0) # going with perform the observation for both teams at same time
-
-        room_data_1 = self.rooms[1].show_cards()
-        observation_1 = Observation(text=room_data_1)       
+    def get_observation(self, agent: Agent, room_num) -> Tuple[Observation, AvailableActions]:
+        room_data = room_num.show_cards() # not perfect, still needs to hide other team info
+        observation = Observation(text=room_data) 
+        identifiers = {card.identifier: card for card in room_data}
 
         available_actions = AvailableActions(
-             instructions = f"Pick a random index from the following list to trade.", # I think it's here, but Leader Instructions would be "return your answer as the index of the item in your room you want to send to the other room"
+             instructions = f"Return your actions as tuples where each integer is between 0 and {self.cards_per_room}-1. Do not pick your own integer.",
              predefined = {
-                ""  
+                 f"{a}": None for a in identifiers # maybe this is the actual available actions
                  },
              openended = {}
         )
-        return observation, available_actions
+        return observation, identifiers, available_actions
 
     def display_rooms(self): 
         for index,room in enumerate(self.rooms):
@@ -126,13 +129,25 @@ class TwoRoomsAndaBoom(Game):
         #pdb.set_trace()
 
         ## Begin playing of rounds ## 
+        player_1 = self.agents[0]
+        player_2 = self.agents[1]
+
+
         print("Begin game")
         self.display_rooms()
         for i in range(3): # always only three rounds, right?
             print(f"Round {i+1}")
-            blue_room_trade = self.rooms[0].cards[2] # this card number will be replaced with the Leader Agent's decision
-            red_room_trade  = self.rooms[1].cards[2]
-            self.trade_card(blue_room_trade, red_room_trade)
+            observation, identifiers, available_actions = self.get_observation(player_1, self.rooms[0])
+            action = player_1.take_action(self.rules, observation, available_actions, show_state=self.show_state)
+            print(action.action_id)
+            room_0_trade = identifiers[action.action_id]
+            
+            observation, identifiers, available_actions = self.get_observation(player_2, self.rooms[1])
+            action = player_2.take_action(self.rules, observation, available_actions, show_state=self.show_state)
+            print(action.action_id)
+            room_1_trade = identifiers[action.action_id]
+
+            self.trade_card(room_0_trade , room_1_trade)
             self.display_rooms()
 
         return determine_winner()
