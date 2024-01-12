c2bbba240818f8618d96b2f702bc5864fb448997
mat <matdallen@gmail.com>
2024-01-12 14:19:28 -0800

    assigning Leader to room

--- games/two_rooms_and_a_boom/two_rooms.py
+++ games/two_rooms_and_a_boom/two_rooms.py
@@ -9,13 +9,14 @@ import pdb
 class TwoRoomsAndaBoom(Game):
    ## Game Classes ##
     class Card:
-        def __init__(self, identifier, team, special_character):
+        def __init__(self, identifier, team, special_character, is_leader):
             self.identifier = identifier #for ease of trading back and forth
             self.team = team # blue / red. Also keeping convention of "Blue / Red" team as ordering
             self.special_character = special_character # Pres / Bomber
+            self.is_leader = is_leader
             
         def __repr__(self):
-            return f"Card({self.identifier}, {self.team}, {self.special_character})"
+            return f"Card({self.identifier}, {self.team}, {self.special_character}, {self.is_leader})"
 
     class Room:
         def __init__(self, identifier):
@@ -72,8 +73,8 @@ class TwoRoomsAndaBoom(Game):
         # assign cards to rooms, special chars, and shuffle rooms
         
         for i in range(1, self.cards_per_room+1):
-            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"{i}", "Blue", "regular"))
-            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"{ i + self.cards_per_room }", "Red", "regular"))
+            self.rooms[0].add_card(TwoRoomsAndaBoom.Card(f"{i}", "Blue", "regular", "not_leader"))
+            self.rooms[1].add_card(TwoRoomsAndaBoom.Card(f"{ i + self.cards_per_room }", "Red", "regular", "not_leader"))
 
         # assign special characters
         self.rooms[0].cards[random.randrange(0, self.cards_per_room)].special_character = "President"
@@ -84,12 +85,26 @@ class TwoRoomsAndaBoom(Game):
             room_0_card = self.rooms[0].cards[random.randrange(0, self.cards_per_room)]
             room_1_card = self.rooms[1].cards[random.randrange(0, self.cards_per_room)]
             self.trade_card(room_0_card, room_1_card) 
+
+        # pick leader at random
+        self.rooms[0].cards[random.randrange(0, self.cards_per_room)].is_leader = "Leader"
+        self.rooms[1].cards[random.randrange(0, self.cards_per_room)].is_leader = "Leader"
+
+        leader_context = ""
+
         return
+
     
     def get_observation(self, agent: Agent, room_num) -> Tuple[Observation, AvailableActions]:
+
+        #leader = [card for card in self.rooms[0].cards if card.is_leader == "Leader"]
+        
+        #self.leader_context += "" 
+
         room_data = room_num.show_cards() # not perfect, still needs to hide other team info
         observation = Observation(text=room_data) 
-        identifiers = {card.identifier: card for card in room_data}
+        # identifiers is used in `predefined` and referencing agent answers; also prevents the Leader from trading themself.
+        identifiers = {card.identifier: card for card in room_data if card.is_leader != "Leader"} 
 
         available_actions = AvailableActions(
              instructions = f"Return your actions as tuples where each integer is between 0 and {self.cards_per_room}-1. Do not pick your own integer.",
@@ -137,16 +152,24 @@ class TwoRoomsAndaBoom(Game):
         self.display_rooms()
         for i in range(3): # always only three rounds, right?
             print(f"Round {i+1}")
+
+            # Room 0
+            leader = [card for card in self.rooms[0].cards if card.is_leader == "Leader"]
             observation, identifiers, available_actions = self.get_observation(player_1, self.rooms[0])
             action = player_1.take_action(self.rules, observation, available_actions, show_state=self.show_state)
-            print(action.action_id)
+#            print(f"{leader} moved the card: ")
+#            print(action.action_id)
             room_0_trade = identifiers[action.action_id]
             
+            # Room 1
+            leader = [card for card in self.rooms[1].cards if card.is_leader == "Leader"]
             observation, identifiers, available_actions = self.get_observation(player_2, self.rooms[1])
             action = player_2.take_action(self.rules, observation, available_actions, show_state=self.show_state)
-            print(action.action_id)
+#            print(f"{leader} moved the card: ")
+#            print(action.action_id)
             room_1_trade = identifiers[action.action_id]
 
+            # Action
             self.trade_card(room_0_trade , room_1_trade)
             self.display_rooms()
 
