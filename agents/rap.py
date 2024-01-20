6ba0a8474450467b298d2e19bce627e52dba270b
Anthony Costarelli <acostarelli@olin.edu>
2024-01-20 23:57:01 +0100

    bug-fixes, mostly typos; variable names could use work

--- agents/rap.py
+++ agents/rap.py
@@ -4,8 +4,9 @@ from api.classes import Action, Agent, AvailableActions, Observation, Rules
 import api.util as util
 import openai
 import math
-from functools import partial
+from functools import cache
 import re
+from typing import NewType, Callable
 
 # These dependencies come from: https://github.com/Ber666/llm-reasoners/tree/main/reasoners
 from agents.reasoners.base import Reasoner, SearchConfig, WorldModel
@@ -20,19 +21,60 @@ from agents.reasoners.algorithm import MCTS
 # * make side-by-side comparison between RAP and LATS, and see how you might
 #   borrow ReACT. Or implement LATS separately (ask Josh first)
 #   * we want CoT reasoning and ability for agent to lookup rules
-# * maybe caching results? It's possible the agent will accurately predict
-#   a future state, in which case it shouldn't recalculate rewards and all.
+# * maybe caching results between calls to take_action? It's possible the agent
+#   will accurately predict a future state, in which case it shouldn't
+#   recalculate rewards and all.
 
-CompletionsFunction = NewType("CompletionsFunction")
-ProbabilitiesFunction = NewType("ProbabilitiesFunction")
+# Todo: tell MCTS to go to this depth
+DEPTH_LIMIT = 2
 
-def expose_openai_api(rules: Rules) -> tuple[CompletionsFunction, ProbabilitiesFunction]:
+"""A context, based on OpenAI API"""
+ContextType = NewType("Context", list[dict[str, str]])
+
+"""Returns the next message in a context."""
+CompletionsFunction = NewType(
+    "CompletionsFunction", Callable[[str, dict[str, str]], str]
+)
+
+"""Determines the probability of tokens appearing next after a context."""
+ProbabilitiesFunction = NewType(
+    "ProbabilitiesFunction",
+    Callable[[str, list[str], int, dict[str, str]], dict[str, float]],
+)
+
+
+def random_replies() -> tuple[CompletionsFunction, ProbabilitiesFunction]:
+    """Returns completions and probabilities that return random responses.
+    Useful for debugging."""
+
+    def randstr():
+        """There is a probability that this returns the same string in different
+        calls which could make debugging confusing."""
+        return "".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=random.randint(5, 10)))
+
+    def completions(template: str, **kwargs) -> str:
+        return f"<state>{randstr()}</state><actions>{randstr()}</actions>"
+
+    def probabilities(
+        template: str, tokens: list[str, str] = ["yes", "no"], n: int = 0, **kwargs
+    ) -> dict[str, float]:
+        return {token: random.random() for token in tokens}
+
+    return completions, probabilities
+
+
+def expose_openai_api(
+    rules: Rules,
+) -> tuple[CompletionsFunction, ProbabilitiesFunction]:
+    """Returns a CompletionsFunction and a ProbabilitiesFunction that
+    interacts with GPT3.5."""
+
     openai_client = openai.Client(
         api_key=util.load_json("credentials.json")["openai_api_key"]
-    )    
+    )
     context_templates = util.load_json("agents\context_templates.json")
 
-    def context_builder(template, **kwargs):
+    def context_builder(template: str, **kwargs) -> ContextType:
         messages = context_templates["prefix"] + context_templates[template]
         return [
             {
@@ -44,7 +86,7 @@ def expose_openai_api(rules: Rules) -> tuple[CompletionsFunction, ProbabilitiesF
             for i, message in enumerate(messages)
         ]
 
-    def completion(template, **kwargs):
+    def completion(template: str, **kwargs):
         context = context_builder(template, **kwargs)
         ret = (
             openai_client.chat.completions.create(
@@ -56,10 +98,8 @@ def expose_openai_api(rules: Rules) -> tuple[CompletionsFunction, ProbabilitiesF
         return ret
 
     def probabilities(
-        template, tokens: list[str] = ["yes", "no"], n: int = 2, **kwargs
+        template: str, tokens: list[str] = ["yes", "no"], n: int = 2, **kwargs
     ) -> dict[str, float]:
-        """Prompt for exactly one token, and return probabilities of each token
-        in tokens."""
         context = context_builder(template, **kwargs)
         n = min(n, 5)  # OpenAI doesn't allow more than 5
 
@@ -76,7 +116,7 @@ def expose_openai_api(rules: Rules) -> tuple[CompletionsFunction, ProbabilitiesF
             .top_logprobs
         )
 
-        def prior(token):
+        def prior(token: str):
             return math.exp(
                 next(
                     (
@@ -94,28 +134,26 @@ def expose_openai_api(rules: Rules) -> tuple[CompletionsFunction, ProbabilitiesF
     return completion, probabilities
 
 
-@dataclass
+@dataclass(frozen=True)
 class GameState:
     observation: str
     depth: int = 0
-    actions: list[str] = None
+    actions: tuple[str] = None
 
 
 @cache
-def step(state: GameState, action: str, context_builder: Callable) -> GameState:
-    prompt = context_builder(
-        "state", observation=state.observation, action=action, others=state.others
+def step(state: GameState, action: str, others: str, completions: CompletionsFunction) -> GameState:
+    new_state = completions(
+        "state", observation=state.observation, action=action, others=others
     )
-    new_state = completion(prompt)
     new_state = re.findall(r"<state>(.*)</state>", new_state, re.S)[0]
     return GameState(new_state, depth=state.depth + 1)
 
 
 @cache
-def win_probability(state: GameState, context_builder: Callable) -> float:
-    prompt = context_builder("goal", observation=state.observation)
-    goal = probabilities(prompt)["yes"]
-    return goal
+def win_probability(state: GameState, probabilities: ProbabilitiesFunction) -> float:
+    winp = probabilities("goal", observation=state.observation)["yes"]
+    return winp
 
 
 @cache
@@ -124,46 +162,63 @@ def is_terminal(state: GameState) -> bool:
 
 
 @cache
-def get_actions(state: GameState, context_builder: Callable) -> list[str]:
+def get_actions(state: GameState, completions: CompletionsFunction) -> tuple[str]:
     if state.actions:
         return state.actions
 
-    prompt = context_builder(
-        "actions",
-        observation=state.observation,
-    )
-    response = completion(prompt)
+    response = completions("actions", observation=state.observation)
     response = re.findall(r"<actions>(.*)</actions>", response, re.S)[0]
     actions = response.strip().split("\n")
-    return actions
+    return tuple(actions)
+
+@cache
+def others_actions(state: GameState, completions: CompletionsFunction) -> str:
+    others = completions("others", observation=state.observation)
+    return others
 
 
 @cache
 def calculate_reward(
-    state: GameState, goal_reached: float, intuition: float, self_eval: float
+    intuition: float, self_eval: float, win_probability: float = 0.5
 ) -> float:
-    goal_reward = 2 * goal_reached - 1
-    return intuition + self_eval + goal_reward
+    win_probability = 2 * win_probability - 1
+    return intuition + self_eval + win_probability
 
 
 @cache
-def intuitions(state: GameState, context_builder: Callable) -> dict[str, float]:
-    prompt = self.context_builder(
+def intuitions(
+    state: GameState, actions: tuple[str], probabilities: ProbabilitiesFunction
+) -> dict[str, float]:
+    ints = probabilities(
         "action_select",
+        actions,
+        len(actions),
         observation=state.observation,
-        actions="\n".join(state.actions),
+        actions="\n".join(actions),
     )
-    ints = probabilities(prompt, action[0], len(state.actions))
     return ints
 
 
+@cache
+def self_eval(state: GameState, action: str, probabilities: ProbabilitiesFunction):
+    val = probabilities("self_eval", observation=state.observation, action=f"{action}")
+    val = val["yes"]
+    return val
+
+
+@dataclass
 class ReasoningViaPlanning(Agent, WorldModel, SearchConfig):
     agent_type_id: str = "rap"
     transparent_reasoning: bool = False
+    use_openai: bool = False
 
     completions: CompletionsFunction = None
     probabilities: ProbabilitiesFunction = None
     _init_state: GameState = None
+
+    def __post_init__(self):
+        mcts = MCTS()
+        self.reasoner = Reasoner(world_model=self, search_config=self, search_algo=mcts)
 
     def take_action(
         self,
@@ -172,12 +227,33 @@ class ReasoningViaPlanning(Agent, WorldModel, SearchConfig):
         available_actions: AvailableActions,
         show_state: bool,
     ) -> Action:
-        self.completions, self.probabilities = expose_openai_api(rules)
+        self.completions, self.probabilities = (
+            expose_openai_api(rules) if self.use_openai else random_replies()
+        )
+
         self._init_state = GameState(
             observation=observation.text,
             depth=0,
-            actions=available_actions.predefined.keys(),
+            actions=tuple(available_actions.predefined.keys()),
         )
+
+        # try:
+        action_id = self.reasoner(None).trace[1][0]
+
+        if self.transparent_reasoning:
+            print(
+                f"RAP: Recieved the following observation:\n{observation.text}\nAvailable actions: {available_actions}.\nChoosing the action: {action_id}"
+            )
+
+        return Action(action_id=action_id)
+        """except Exception as e:
+            if self.transparent_reasoning:
+                print(
+                    f"RAP: MCTS threw an error: {e}. Likely, the internal depth limit was reached and no terminal state was found."
+                )
+
+            actions = list(available_actions.predefined.keys())
+            return Action(action_id=random.choice(actions))"""
 
     def init_state(self) -> GameState:
         if self.transparent_reasoning:
@@ -186,19 +262,20 @@ class ReasoningViaPlanning(Agent, WorldModel, SearchConfig):
         return self._init_state
 
     def step(self, state: GameState, action: str) -> tuple[GameState, dict[str, float]]:
-        next = step(state, action, self.completions)
-        goal = win_probability(next, self.probabilities)
-        info = {"goal_reached": goal}
+        oth = others_actions(state, self.completions)
+        nxt = step(state, action, oth, self.completions)
+        winp = win_probability(nxt, self.probabilities)
+        info = {"win_probability": winp}
 
         if self.transparent_reasoning:
             print(
-                f"RAP: Stepping from\n{state.observation}\nwith action {action}. New state looks like\n{next.observation}"
+                f"RAP: Stepping from\n{state.observation}\nwith action {action}. New state looks like\n{nxt.observation}"
             )
 
-        return next, info
+        return nxt, info
 
-    def is_terminal(self, state: GameWrapper.State) -> bool:
-        term = is_terminal(state))
+    def is_terminal(self, state: GameState) -> bool:
+        term = is_terminal(state)
 
         if self.transparent_reasoning:
             print(
@@ -207,7 +284,7 @@ class ReasoningViaPlanning(Agent, WorldModel, SearchConfig):
 
         return term
 
-    def get_actions(self, state: GameWrapper.State) -> list[str]:
+    def get_actions(self, state: GameState) -> list[str]:
         actions = get_actions(state, self.completions)
 
         if self.transparent_reasoning:
@@ -220,16 +297,18 @@ class ReasoningViaPlanning(Agent, WorldModel, SearchConfig):
     def fast_reward(
         self, state: GameState, action: str
     ) -> tuple[float, dict[str, float]]:
-        int = intuitions(state, self.probabilities)[action]
+        actions = get_actions(state, self.completions)
+        int = intuitions(state, actions, self.probabilities)[action]
         sev = self_eval(state, action, self.probabilities)
-        rew = calculate_reward(int, sev, goal_reached)
+        rew = calculate_reward(int, sev)
+        info = {"intuition": int, "self_eval": sev}
 
         if self.transparent_reasoning:
             print(
-                f"RAP: Calculating fast reward for the following state:\n{state.observation}\nAction: {action}\nIntuition: {int}, Self-eval: {sev}, Reward: {reward}"
+                f"RAP: Calculating fast reward for the following state:\n{state.observation}\nAction: {action}\nIntuition: {int}, Self-eval: {sev}, Reward: {rew}"
             )
 
-        return rew
+        return rew, info
 
     def reward(
         self,
@@ -237,14 +316,14 @@ class ReasoningViaPlanning(Agent, WorldModel, SearchConfig):
         action: str,
         intuition: float,
         self_eval: float,
-        goal_reached: float,
+        win_probability: float,
     ) -> tuple[float, dict[str, float]]:
-        rew = calculate_reward(intuition, self_eval, goal_reached)
-        info = {"intuition": intuition, "goal_reached": goal_reached}
+        rew = calculate_reward(intuition, self_eval, win_probability)
+        info = {"intuition": intuition, "win_probability": win_probability}
 
         if self.transparent_reasoning:
             print(
-                f"RAP: Calculating fast reward for the following state:\n{state.observation}\nAction: {action}\nIntuition: {int}, Self-eval: {sev}, Goal-reached: {goal_reached}, Reward: {reward}"
+                f"RAP: Calculating fast reward for the following state:\n{state.observation}\nAction: {action}\nIntuition: {intuition}, Self-eval: {self_eval}, Win-probability: {win_probability}, Reward: {rew}"
             )
 
         return rew, info
