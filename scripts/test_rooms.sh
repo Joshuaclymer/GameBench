5cd5e0d0d7522d83943dc0eb8cdf8dc24751192d
doomdagadiggiedahdah <77366355+doomdagadiggiedahdah@users.noreply.github.com>
2024-01-19 16:11:58 -0800

    Delete scripts/test_rooms.sh

--- scripts/test_rooms.sh
+++
@@ -1,6 +0,0 @@
-python api/play_game.py \
-    --agent_1_path agents.gpt.ChatGPTText \
-    --agent_2_path agents.gpt.ChatGPTText \
-    --game_path games.two_rooms_and_a_boom.two_rooms.TwoRoomsAndaBoom \
-    --save_results False
-# same agents for testing since cheap
