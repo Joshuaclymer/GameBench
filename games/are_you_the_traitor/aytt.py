91a5b2faa1dfa398d94b6e1babfb9b588e4df320
mat <matdallen@gmail.com>
2024-01-24 16:19:48 -0800

    added players, assigning teams, check_if_winner(), return winning values

--- games/are_you_the_traitor/aytt.py
+++ games/are_you_the_traitor/aytt.py
@@ -4,46 +4,71 @@ import random
 
 @dataclass
 class AreYouTheTraitor(Game):
+
+    #########################
+    ### Class definitions ###
+    #########################
+    class Player:
+        def __init__(self, identifier, team, score, agent):
+            self.identifier = identifier
+            self.team = team
+            self.score = score
+            self.agent = agent
+
+        def __repr__(self):
+            return f"Player({self.identifier}, {self.team}, {self.score})"
+
+    #############################
+    ### Attribute definitions ###
+    #############################
     rules : Rules = Rules(
         title="Are you the traitor?",
         summary="The Good team wants to destroy an Evil Magic Key while the Evil team wants to keep it. The key can be destroyed by giving it to the Good Wizard, but there is an Evil Wizard who looks exactly alike. Use social deduction to find out who is who, but also know that there is a traitor among the guards who have the key.",
         additional_details = None
     )
     id : str = "are_you_the_traitor"
+    list_all_players : list = field(default_factory=list)
+    players_per_team : int = 3
 
     def init_game(self, agent1 : Agent, agent2 : Agent, show_state : bool):        
-        
         self.agents = [agent1(team_id = 0, agent_id = 0), agent2(team_id = 1, agent_id = 1)]
-
         self.winning_team = None
-
         self.show_state = show_state
+        
+
+        ######################
+        ### create players ###
+        ######################
+
+        for i in range(1, self.players_per_team+1):
+            self.list_all_players.append(self.Player(i, "Good", 0, self.agents[0]))
+            self.list_all_players.append(self.Player(i+self.players_per_team, "Evil", 0, self.agents[1]))
         return
     
     def get_observation(self, agent : Agent): 
         return
 
     def play(self):
-        if self.show_state: print("test show_state")
+        #if self.show_state: print("test show_state")
         player_1 = self.agents[0]
         player_2 = self.agents[1]
         
-        
-        # while True:
-        #     # Player 1 moves
-        #     observation, available_actions = self.get_observation(player_1)
-        #     action = player_1.take_action(self.rules, observation, available_actions, show_state=self.show_state)
-        #     self.update(action, available_actions, player_1)
-        #     if self.game_is_over:
-        #         break
+        def check_if_winner():
+            #print(self.list_all_players[1].score)
+            for i in self.list_all_players:
+                if i.score >= 10:
+                    if self.show_state: print("game done")
+                    self.winning_team = i.team
+                    return True
+                else:
+                    continue
+            return False
 
-        #     # Player 2 moves
-        #     observation, available_actions = self.get_observation(player_2)
-        #     action = player_2.take_action(self.rules, observation, available_actions, show_state=self.show_state)
-        #     self.update(action, available_actions, player_2)
-        #     if self.game_is_over:
-        #         break
+        while check_if_winner() == False:
+            self.list_all_players[1].score += 1
+            continue
 
-        self.winning_team = None
+        print(f"The {self.winning_team} team is the winner")
 
-        return (0.5, 0.5) if self.winning_team == None else (float(self.winning_team == 0), float(self.winning_team == 1))
+        # I'm not convinced that this works with the outside program appropriately
+        return (1, 0) if self.winning_team == "Good" else (0,1)
