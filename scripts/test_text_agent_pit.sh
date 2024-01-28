f1fe715f1a8d6d3dfeb12ba67cec2d624fc11ad2
y-arjun-y <arjyadav7@gmail.com>
2024-01-28 21:34:55 +0400

    finished checklist: added script

---
+++ scripts/test_text_agent_pit.sh
@@ -0,0 +1,6 @@
+python api/play_game.py \
+    --agent_1_path agents.random_agent.RandomAgent \
+    --agent_2_path agents.gpt.GPT4Text \
+    --game_path games.pit.TicTacToe \
+    --show_state \
+    --num_matches 50
