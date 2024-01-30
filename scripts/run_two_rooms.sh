9a06329fd49392f307ce04bb0c23b2f044d73f76
mat <matdallen@gmail.com>
2024-01-30 11:40:15 -0800

    ready for PR

---
+++ scripts/run_two_rooms.sh
@@ -0,0 +1,6 @@
+python ./api/play_game.py \
+    --agent_1_path agents.gpt.ChatGPTText \
+    --agent_2_path agents.gpt.ChatGPTText \
+    --game_path games.two_rooms_and_a_boom.two_rooms.TwoRoomsAndaBoom \
+    --save_results False \
+    --show-state False
