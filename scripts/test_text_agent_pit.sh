83a76aff6188d39b53745aa31a3ba5b869f60158
y-arjun-y <arjyadav7@gmail.com>
2024-02-16 17:22:55 +0400

    edited script

--- scripts/test_text_agent_pit.sh
+++ scripts/test_text_agent_pit.sh
@@ -1,6 +1,6 @@
-python api/play_game.py \
+python3 api/play_game.py \
     --agent_1_path agents.random_agent.RandomAgent \
     --agent_2_path agents.gpt.GPT4Text \
-    --game_path games.pit.TicTacToe \
+    --game_path games.pit.PitGame \
     --show_state \
     --num_matches 50
