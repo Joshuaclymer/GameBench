357325e861abf93bc2e8922c713cebf989ab4bf4
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-02-22 15:24:52 -0800

    Add GPT-4 vs. random agent Santorini test script

---
+++ scripts/test_santorini.sh
@@ -0,0 +1,6 @@
+python api/play_game.py \
+    --agent_1_path agents.random_agent.RandomAgent \
+    --agent_2_path agents.gpt.GPT4Text \
+    --game_path games.santorini.santorini.Santorini \
+    --show_state \
+    --num_matches 3
