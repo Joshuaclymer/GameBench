919b991a593dcdaa45c728e620a42374c9f02261
Grace <64430320+grace-sodunke@users.noreply.github.com>
2024-02-25 12:51:26 +0000

    Update arctic_scavengers.py

--- games/arctic_scavengers/arctic_scavengers.py
+++ games/arctic_scavengers/arctic_scavengers.py
@@ -2,9 +2,23 @@ from dataclasses import dataclass, field
 from api.classes import Observation, Action, Agent, AvailableActions, Game, Rules
 from typing import List, Dict, Optional, Tuple
 from cards.game_cards import *
-
+import random
+ 
 @dataclass
 class ArcticScavengers(Game):
+    class Player:
+        def __init__(self, agent):
+            self.agent = agent
+            self.cards = {"deck":[], "draw":[], "skirmish":[]} 
+
+        def create_deck(self, deck):
+            pass
+    class Deck:
+        def __init__(self):
+            self.contested_resources = []
+            self.junkyard = []
+            self.mercenaries = [[]] # 8 mercenary piles
+
     rules : Rules = Rules(
         title="Arctic Scavengers",
         summary="""
@@ -18,14 +32,11 @@ class ArcticScavengers(Game):
 
     def init_game(self, agent1 : Agent, agent2 : Agent):
         self.agents = [agent1(team_id = 0, agent_id = 0, **self.agent_1_kwargs), agent2(team_id = 1, agent_id = 1, **self.agent_2_kwargs)]
-        
-        self.cards_player0 = {"deck":[], "draw":[], "skirmish":[]}
-        self.cards_player1 = {"deck":[], "draw":[], "skirmish":[]}
-
+        self.players = [self.Player(self.agents[0]), self.Player(self.agents[1])]
+        self.deck = self.Deck()
         # TODO: Initialise 10 cards for each player (4 refugee, 3 scavenger, 1 brawler, 1 spear, 1 shovel)
-        # Shuffle deck for each player. Randomly choose player 0 or 1 to be initiator.
-        # Create remaining piles for: contested resources, junkyard, and 8 mercenary piles.
-
+        for player in self.players:
+            player.create_deck(self.deck)
         self.game_winner = None
     
     def get_observation(self, agent : Agent) -> Tuple[Observation, AvailableActions]:
@@ -38,3 +49,12 @@ class ArcticScavengers(Game):
         # 14 turns in total, as there are 14 contested resource cards in the pile.
         # DRAWING PHASE: 
         # Both players draw 5 cards from top of their deck. If deck is empty, shuffle discard pile and draw from there.
+        count = 0
+        while self.deck.contested_resources:
+            initiator = count % 2
+            # Draw 5 cards automatically for each player. get_observation returns the hand of the player.
+            # The available actions are to choose which cards to keep for skirmish.
+            # Call get_observation again to decide which resources to gather. Need to use logic to check validity (otherwise call again).
+            # Call update again to update the player's deck.
+            # Then the skirmish happens, and the top contested resource card goes to the winining player.
+            count += 1
