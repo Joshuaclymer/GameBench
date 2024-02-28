28fc81aae1206c0c15068d2aff479222cd4389f3
Suhas Hariharan <hariharansuhas@gmail.com>
2024-02-28 23:02:28 +0000

    account for coordinate system switch

--- games/hive/pieces.py
+++ games/hive/pieces.py
@@ -21,7 +21,7 @@ class HivePiece(ABC):
 class QueenBee(HivePiece):
     def __init__(self, owner):
         super().__init__("QueenBee", owner)
-
+    
     def valid_moves(self, board):
         current_hex = self.find_current_hex(board)
         if current_hex is None:
@@ -46,20 +46,62 @@ class Spider(HivePiece):
         if start_hex is None:
             return []
 
-        return self.find_moves(start_hex, board, 3)
+        visited = set([start_hex])  # Initialize the visited set with the starting hex
+        valid_moves = self.find_moves(start_hex, start_hex, board, 3, visited)
+        return valid_moves
+    
+    def has_common_neighboring_piece(self, hex1, hex2, board):
+        neighbors1 = self.get_neighbors_with_pieces(hex1, board) 
+        neighbors2 = self.get_neighbors_with_pieces(hex2, board)
+        return any(neighbor in neighbors2 for neighbor in neighbors1)
 
-    def find_moves(self, current_hex, board, steps_remaining):
+    def get_neighbors_with_pieces(self, hex, board):
+        neighbors_with_pieces = set()
+        for direction in range(6):
+            neighbor_hex = hex.neighbor(direction)
+            if neighbor_hex in board.board:
+                neighbors_with_pieces.add(neighbor_hex)
+        return neighbors_with_pieces
+
+    def find_moves(self, start_hex, current_hex, board, steps_remaining, visited):
         if steps_remaining == 0:
-            return [current_hex]
-
+            # If the spider has moved 3 steps, return the current hex as a possible move,
+            # but only if it is not the starting hex (which is already in visited)
+            print(current_hex, [str(i) for i in visited])
+            return [current_hex] if current_hex not in visited else []
+        new_visited = visited.union({current_hex})
         moves = []
         for direction in range(6):
             neighbor_hex = current_hex.neighbor(direction)
-            if board.can_move_piece(current_hex, neighbor_hex):
-                further_moves = self.find_moves(neighbor_hex, board, steps_remaining - 1)
-                moves.extend([move for move in further_moves if move != current_hex])
+
+            is_current_hex_real = current_hex in board.board
+
+            if not is_current_hex_real:
+                temp_spider = Spider(0)
+                board.board[current_hex] = temp_spider
+                backup_old_spider = board.board[start_hex]
+                del board.board[start_hex]
+
+            if board.can_move_piece(current_hex, neighbor_hex) and neighbor_hex not in visited and self.has_common_neighboring_piece(current_hex, neighbor_hex, board):
+                # Add the neighbor to the visited set to prevent backtracking in this move sequence
+                if not is_current_hex_real:
+                    is_current_hex_real = True
+                    del board.board[current_hex]
+                    board.board[start_hex] = backup_old_spider
+                further_moves = self.find_moves(start_hex, neighbor_hex, board, steps_remaining - 1, new_visited)
+                moves.extend(further_moves)
+
+            if not is_current_hex_real:
+                del board.board[current_hex]
+                board.board[start_hex] = backup_old_spider
 
         return list(set(moves))  # Removing duplicates
+
+    def find_current_hex(self, board):
+        for hex, piece in board.board.items():
+            if piece is self:
+                return hex
+        return None
 
     def find_current_hex(self, board):
         for hex, piece in board.board.items():
@@ -79,20 +121,27 @@ class Grasshopper(HivePiece):
         valid_moves = []
         for direction in range(6):
             jump_hex = self.jump_over_pieces(board, current_hex, direction)
-            if jump_hex and board.can_move_piece(current_hex, jump_hex):
-                valid_moves.append(jump_hex)
-
+            if jump_hex is not None:
+                if board.can_move_piece(current_hex, jump_hex):
+                    valid_moves.append(jump_hex)
         return valid_moves
 
     def jump_over_pieces(self, board, start_hex, direction):
         """
-        Jump over the pieces in the given direction and return the landing hex.
+        Jump over the pieces in the given direction and return the landing hex. 
+        At least one piece must be jumped over. Check if the hex is within the bounds of the board.
         """
         current_hex = start_hex.neighbor(direction)
-        while board.get_piece_at(current_hex) is not None:
+        jumped_over_piece = False
+        
+        # Continue jumping over pieces until an empty hex is found
+        while current_hex and board.get_piece_at(current_hex) is not None:
             current_hex = current_hex.neighbor(direction)
+            jumped_over_piece = True
 
-        return current_hex if current_hex != start_hex else None
+        # Ensure that we have jumped at least one piece and the landing hex is not the same as the starting hex
+        return current_hex if current_hex and current_hex != start_hex and jumped_over_piece else None
+
 
 class SoldierAnt(HivePiece):
     def __init__(self, owner):
@@ -102,7 +151,7 @@ class SoldierAnt(HivePiece):
         current_hex = self.find_current_hex(board)
         if current_hex is None:
             return []
-
+    
         return self.find_moves(current_hex, board, set(), current_hex)
 
     def find_moves(self, current_hex, board, visited, start_hex):
@@ -117,11 +166,33 @@ class SoldierAnt(HivePiece):
 
         visited.add(current_hex)
         moves = []
+
+
         for direction in range(6):
             neighbor_hex = current_hex.neighbor(direction)
+            
+            temp_ant_created = False
+
+            if current_hex not in board.board:
+                temp_ant = SoldierAnt(0)
+                board.board[current_hex] = temp_ant
+                backup_old_ant = board.board[start_hex]
+                temp_ant_created = True
+                del board.board[start_hex]
+
             if board.can_move_piece(current_hex, neighbor_hex) and neighbor_hex != start_hex:
                 moves.append(neighbor_hex)
+                if temp_ant_created:
+                    del board.board[current_hex]
+                    board.board[start_hex] = backup_old_ant
+                    temp_ant_created = False
                 moves.extend(self.find_moves(neighbor_hex, board, visited, start_hex))
+
+            if temp_ant_created:
+                del board.board[current_hex]
+                board.board[start_hex] = backup_old_ant
+                temp_ant_created = False
+
 
         return list(set(moves))
     
