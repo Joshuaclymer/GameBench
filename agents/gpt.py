17b18353e38536332558fda185d5e53a718a73c2
Anthony Costarelli <acostarelli@olin.edu>
2024-03-20 20:14:34 +0100

    Really hacky way to keep track of tokens

--- agents/gpt.py
+++ agents/gpt.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass, field
 from api.classes import Agent, AvailableActions, Action, Observation, Rules
 import random
@@ -31,6 +32,15 @@ openai_client = openai.Client(
     api_key=util.load_json("credentials.json")["openai_api_key"]
 )
 
+tokens = defaultdict(int)
+def completions(*args, **kwargs):
+    ret = openai_client.chat.completions.create(*args, **kwargs)
+
+    model = kwargs["model"]
+    tokens[f"{model}_input"] += ret.usage.prompt_tokens
+    tokens[f"{model}_output"] += ret.usage.completion_tokens
+    print(tokens)
+    return ret
 
 @dataclass
 class OpenAITextAgent(Agent):
@@ -90,7 +100,7 @@ class OpenAITextAgent(Agent):
                 buffered = BytesIO()
                 image.save(buffered, format="JPEG")
                 base64_image = base64.b64encode(buffered.getvalue())
-                imagedesc = openai_client.chat.completions.create(
+                imagedesc = completions(
                     model="gpt-4-vision-preview",
                     messages=[
                         {
@@ -143,7 +153,7 @@ class OpenAITextAgent(Agent):
             messages.append({"role": "user", "content": prompt})
 
             response = (
-                openai_client.chat.completions.create(
+                completions(
                     model=self.openai_model
                     if observation.image is None
                     else "gpt-4-vision-preview",
@@ -164,7 +174,7 @@ class OpenAITextAgent(Agent):
 
             messages.append({"role": "user", "content": prompt})
             response = (
-                openai_client.chat.completions.create(
+                completions(
                     model=self.openai_model
                     if observation.image is None
                     else "gpt-4-vision-preview",
@@ -187,7 +197,7 @@ class OpenAITextAgent(Agent):
         result = None
         for _ in range(self.max_retries):
             response = (
-                openai_client.chat.completions.create(
+                completions(
                     model=self.openai_model
                     if observation.image is None
                     else "gpt-4-vision-preview",
