db916d3a1f09a2e5bdcf364890f63e8904f7eb13
Suhas Hariharan <hariharansuhas@gmail.com>
2024-03-25 21:52:31 +0000

    add validation on num_guesses

--- games/codenames/game.py
+++ games/codenames/game.py
@@ -161,7 +161,11 @@ class CodenamesGame(Game):
             raise ValueError("Agent is not a spymaster.")
 
         if action.action_id == "submit_clue":
-            clue, num_guesses = action.openended_response.split(",")
+            try:
+                clue, num_guesses = action.openended_response.split(",")
+            except ValueError:
+                clue = "None"
+                num_guesses = 1
             num_guesses = int(num_guesses)
             if num_guesses < 0:
                 raise ValueError("Number of guesses must be non-negative.")
