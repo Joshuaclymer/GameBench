200f53e530e841638f861209e3a9492a8f19823b
Anthony Costarelli <acostarelli@olin.edu>
2024-04-09 10:58:35 +0200

    other games need a string here to work; hopefully these changes dont break other games

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -8,4 +8,4 @@ class RandomAgent(Agent):
 
     def take_action(self, rules : Rules, observation: Observation, available_actions: AvailableActions, show_state : bool):
         actions = list(available_actions.predefined.keys()) + list(available_actions.openended.keys())
-        return Action(action_id=random.choice(actions))
+        return Action(action_id=random.choice(actions), openended_response="")
